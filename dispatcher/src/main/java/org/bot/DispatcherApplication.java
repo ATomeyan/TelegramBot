2cd794bfe0dceb2b0d584c3d5188816b19ccc22c
arturto <tomeyan.artur@gmail.com>
2022-10-20 18:37:37 +0400

    Initial

---
+++ dispatcher/src/main/java/org/bot/DispatcherApplication.java
@@ -0,0 +1,15 @@
+package org.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Artur Tomeyan
+ * @date 20/10/2022
+ */
+@SpringBootApplication
+public class DispatcherApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DispatcherApplication.class);
+    }
+}
---
+++ dispatcher/src/main/java/org/bot/controller/TelegramBot.java
@@ -0,0 +1,27 @@
+package org.bot.controller;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+/**
+ * @author Artur Tomeyan
+ * @date 20/10/2022
+ */
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
