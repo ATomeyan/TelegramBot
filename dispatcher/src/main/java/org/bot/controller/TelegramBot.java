79a01fb7bc34f7dcfa0049f82085ab537b36e7c0
arturto <tomeyan.artur@gmail.com>
2022-10-25 15:20:56 +0400

    Initial

--- dispatcher/src/main/java/org/bot/controller/TelegramBot.java
+++ dispatcher/src/main/java/org/bot/controller/TelegramBot.java
@@ -1,27 +1,57 @@
 package org.bot.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 /**
  * @author Artur Tomeyan
  * @date 20/10/2022
  */
 @Component
+
 public class TelegramBot extends TelegramLongPollingBot {
+
+    private static final Logger logger = Logger.getLogger(TelegramBot.class);
+    @Value("${bot.name}")
+    private String botName;
+
+    @Value("${bot.token}")
+    private String botToken;
+
     @Override
     public String getBotUsername() {
-        return null;
+        return botName;
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return botToken;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        logger.debug(message.getText());
 
+        SendMessage response = new SendMessage();
+        response.setChatId(message.getChatId().toString());
+        response.setText("Hello from BOT");
+        sendAnswerMessage(response);
+    }
+
+    private void sendAnswerMessage(SendMessage sendMessage){
+        if (sendMessage != null){
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e){
+                logger.error(e);
+            }
+        }
     }
 }
